72d07fd54b0fc3bcb53a82aa3f90647b0235c8e5
Frhanarka <frhanarka@gmail.com>
2026-04-07 10:50:41 +0700

    prosedur pengecekan kondisi

--- switchcase.cpp
+++ switchcase.cpp
@@ -15,3 +15,25 @@ void input(){
     cout << "Masukkan Kode Jurusan (1-3): ";
     cin >> KodeJurusan;
 }
+
+//prosedur untuk pengecekan kondisi
+
+void pilihan(){
+    switch (KodeJurusan) {
+        case 1:
+            NamaJurusan = "Teknologi Informasi";
+            statuskuota = "Tersedia (15 kursi)";
+            break;
+        case 2:
+            NamaJurusan = "Teknik Elektro";
+            statuskuota = "kuota penuh";
+            break;
+        case 3:
+            NamaJurusan = "Sistem Informasi";
+            statuskuota = "Tersedia (5 kursi)";
+            break;
+        default:
+            NamaJurusan = "Tidak diketahui";
+            statuskuota = "Error: kode tidak valid";
+    }
+}
