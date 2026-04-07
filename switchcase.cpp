c1577d2d33136ea62c2992ab55fff684da3db109
Frhanarka <frhanarka@gmail.com>
2026-04-07 10:45:47 +0700

    input jurusan kampus

--- switchcase.cpp
+++ switchcase.cpp
@@ -4,3 +4,14 @@ using namespace std;
 int KodeJurusan;
 string Jurusan;
 string NamaJurusan;
+
+// prosedur input
+void input(){
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS UMY ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "Masukkan Kode Jurusan (1-3): ";
+    cin >> KodeJurusan;
+}
